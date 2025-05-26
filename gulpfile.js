ab5b411ab1816683817a43b4dc99463b6eaf66e5
Ryota-Ito0730 <rgitmain01@gmail.com>
2025-05-26 11:31:33 +0900

    first commit

---
+++ gulpfile.js
@@ -0,0 +1,91 @@
+import gulp from "gulp";
+const {src, dest, watch, series, parallel} = gulp;
+
+// Browser Sync
+import browserSync from "browser-sync";
+
+// ブラウザをリロードするタスク
+const browserSyncReload = (done) => {
+  browserSync.reload(); // BrowserSyncを使ってブラウザをリロードする
+  done();
+}
+
+const browserSyncStart = (done) => {
+  browserSync.init({
+    server: {
+      baseDir: "./dist", // ルートとなるディレクトリを指定
+    },
+    startPath: "index.html", 
+    notify: true,// 画面右上の通知の有無
+  });
+  done();
+}
+
+// Pugのコンパイル用プラグイン
+import pug from "gulp-pug";
+
+// SassをDartSassでコンパイル
+import * as dartSass from "sass";
+import gulpSass from "gulp-sass";
+const sass = gulpSass(dartSass);
+
+// webpに変更せずに圧縮する場合は、下記を有効にします
+import imagemin from "gulp-imagemin";
+import optipng from "imagemin-pngquant";
+import mozjpeg from "imagemin-mozjpeg";
+
+//webpに変換します
+// import webp from "gulp-webp";
+
+// 各タスクで指定するパスを読み込み
+import pathObj from "./gulpfilePathConfig.js";
+
+/* Sass(SCSS)をコンパイルするタスク
+ */
+const compileSass = () => {
+  return src(pathObj.sass.src)
+    .pipe(sass({ outputStyle: "expanded" }))
+    .pipe(dest(pathObj.sass.dist))
+    .pipe(browserSync.stream());
+};
+
+/**
+ * Pugをコンパイルするタスク
+ */
+const compilePug = () => {
+  return src(pathObj.pug.src)
+    .pipe(pug({ pretty: true }))
+    .pipe(dest(pathObj.pug.dist))
+};
+
+/**
+ * 画像を圧縮します
+ */
+const convertImage = () => {
+  return src(pathObj.img.src,{encoding: false})
+    .pipe(imagemin([
+      mozjpeg({quality: 75, progressive: true}),
+      optipng({optimizationLevel: 5}),
+    ]))
+    .pipe(dest(pathObj.img.dist))
+};
+
+// Webpに変換する場合は、上記タスクは無効化し、下記を有効化します
+// const convertImage = () => {
+// 	return src(pathObj.img.src,{encoding: false})
+//     .pipe(webp({quality: 50}))
+//     .pipe(dest(pathObj.img.distWebp))
+// };
+
+/**
+ * 各ファイルを監視し、変更があったらSassやHTMLを変換するタスク
+ */
+const watchFiles = () => {
+  watch(pathObj.sass.src, compileSass);
+  watch(pathObj.pug.src, series(compilePug,browserSyncReload));
+  watch(pathObj.img.src, series(convertImage,browserSyncReload));
+};
+
+export default parallel(browserSyncStart, watchFiles);
+
+
---
+++ gulpfilePathConfig.js
@@ -0,0 +1,25 @@
+/**
+ * gulpfile.js内の各タスクで指定するパスを本ファイルに集約します
+ * 注意:本ファイルはgulpfile.jsでのみ使用します
+ * 
+ * _src:開発中の参照先や監視先を指定します
+ * dist:公開用ファイルの出力先を指定します
+ */
+
+const pathObj = {
+  sass : {
+    src: "./_src/assets/scss/*.scss",
+    dist : "./dist/assets/css/"
+  },
+  pug : {
+    src: ["./_src/*.pug","./_src/**/*.pug"],
+    dist : "./dist/"
+  },
+  img : {
+    src: "./_src/assets/img/*.{jpg,jpeg,png}",
+    dist : "./dist/assets/img/", // 拡張子の変更はしない場合に指定
+    distWebp : "./dist/assets/img/webp" // WebPへ変換する場合に指定
+  },
+}
+
+export default pathObj;
